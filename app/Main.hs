1cd95a5b3a2dd8e791878184f4abfbcda87edcad
prajnastra <coder.abhijit90@gmail.com>
2022-12-24 16:19:04 +0530

    finish full app

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,77 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import System.Environment
+import System.Directory
+import System.IO
+import Control.Exception
+import Data.List
+
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+dispatch "view" = view
+dispatch "remove" = remove
+dispatch "bump" = bump
+dispatch command = doesNotExist command
+
+main = do
+    (command:argList) <- getArgs
+    dispatch command argList
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+add _ = putStrLn "The add command takes exactly two arguments"
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                    [0..] todoTasks
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                    [0..] todoTasks
+    putStrLn "These are your TO-DO items: "
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile "todo.txt"
+            renameFile tempName "todo.txt")
+
+bump :: [String] -> IO ()
+bump [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line)
+                                    [0..] todoTasks
+    putStrLn "These are your TO-DO items: "
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        bumpLine = (todoTasks !! number)
+        newTodoTasks = delete (todoTasks !! number) todoTasks
+        newTodoItems = unlines $ bumpLine:newTodoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile "todo.txt"
+            renameFile tempName "todo.txt")
+
+doesNotExist :: String -> [String] -> IO ()
+doesNotExist command _ = 
+    putStrLn $ "The " ++ command ++ " command doesn't exist"
